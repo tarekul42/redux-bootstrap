990611defeab0b667a2995fad4c84d0170ab5c4d
tarekul42 <tarekulrifat205@gmail.com>
2025-06-24 22:16:50 +0600

    added data with action payloads

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   const dispatch = useAppDispatch();
   const { count } = useAppSelector((state) => state.counter);
 
-  const handleIncrement = () => {
-    dispatch(increment());
+  const handleIncrement = (amount: number) => {
+    dispatch(increment(amount));
   };
 
   const handleDecrement = () => {
@@ -16,7 +16,20 @@ function App() {
   return (
     <div>
       <h1>Counter with Redux</h1>
-      <button onClick={handleIncrement}>Increment</button>
+      <button
+        onClick={() => {
+          handleIncrement(5);
+        }}
+      >
+        Increment by 5
+      </button>
+      <button
+        onClick={() => {
+          handleIncrement(1);
+        }}
+      >
+        Increment
+      </button>
       <div>{count}</div>
       <button onClick={handleDecrement}>Decrement</button>
     </div>
--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -8,8 +8,8 @@ const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
-    increment: (state) => {
-      state.count = state.count + 1;
+    increment: (state, action) => {
+      state.count = state.count + action.payload;
     },
     decrement: (state) => {
       state.count = state.count - 1;
