16c70dc1dfa3556ab658f140924035e0f927c2b6
tarekul42 <tarekulrifat205@gmail.com>
2025-06-24 20:52:51 +0600

    first try with redux

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Counter with Redux</h1>
+      <button>Increment</button>
+      <div>0</div>
+      <button>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
