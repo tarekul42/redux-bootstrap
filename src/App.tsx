f891f42f61b6b785598dca62638c69c1bc25af36
tarekul42 <tarekulrifat205@gmail.com>
2025-06-24 21:51:55 +0600

    created first slice and actions in redux and used use State

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+
 function App() {
+  const dispatch = useAppDispatch();
+  const { count } = useAppSelector((state) => state.counter);
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
     <div>
       <h1>Counter with Redux</h1>
-      <button>Increment</button>
-      <div>0</div>
-      <button>Decrement</button>
+      <button onClick={handleIncrement}>Increment</button>
+      <div>{count}</div>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
 }
--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -7,7 +7,16 @@ const initialState = {
 const counterSlice = createSlice({
   name: "counter",
   initialState,
-  reducers: {},
+  reducers: {
+    increment: (state) => {
+      state.count = state.count + 1;
+    },
+    decrement: (state) => {
+      state.count = state.count - 1;
+    },
+  },
 });
+
+export const { increment, decrement } = counterSlice.actions;
 
 export default counterSlice.reducer;
