52627653cd12c6cc5069b773a6cddd4db7dd9354
Tarekul Islam Rifat <121659071+tarekul42@users.noreply.github.com>
2025-06-26 22:39:21 +0600

    Update hook.ts

--- src/redux/hook.ts
+++ src/redux/hook.ts
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "./store";
 
+// hooks are added here
 export const useAppSelector = useSelector.withTypes<RootState>();
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
